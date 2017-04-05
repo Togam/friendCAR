0bc57cef54475f4c37f4cd3ffe6358bef9e5abb6
six <six@a15p16.fil.univ-lille1.fr>
2017-04-05 15:07:19 +0200

    page service Inscription commencé

---
+++ friendCAR/src/services/Inscription.java
@@ -0,0 +1,96 @@
+package services;
+
+import persistance.DBconfig;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+@Path("/inscription")
+public class Inscription {
+
+	@Context
+	private UriInfo context;
+
+	/**
+	 * Creates a new instance of ServiceResource
+	 */
+	public Inscription() {
+	}
+
+	/**
+	 * Retrieves representation of an instance of com.example.ServiceResource
+	 *
+	 * @return an instance of java.lang.String
+	 */
+	@GET
+	@Produces("text/html")
+	public String connectionForm() {
+		try {
+			return "<html>"
+					+"<body>"
+					+"<h1>\" FriendCAR, le réseau social des geeks! </h1>" 
+					+"<meta charset=\"utf-8\">"
+					+"<div class=\"tab-content-inner\" data-content=\"signup\">"
+					+"<form method=\"post\" action=\"fonctions/gestion_inscritption.php\">"
+					+"<div class=\"col-md-12\">"
+					+"<label for=\"username\">Pseudo : </label>"
+					+"<input type=\"text\" class=\"form-control\" name=\"username\">"
+					+"</div>"
+					+"</br>"
+					+"<div class=\"col-md-12\">"
+					+"<label for=\"password\">Mot de Passe : </label>"
+					+"<input type=\"password\" class=\"form-control\" name=\"password\">"
+					+"</div>"
+					+"</br>"
+					+"<div class=\"col-md-12\">"
+					+"<label for=\"password2\">Confirmez Mot de Passe : </label>"
+					+"<input type=\"password\" class=\"form-control\" name=\"password2\">"
+					+"</div>"
+					+"</br>"
+					+"<div class=\"col-md-12\">"
+					+"<label for=\"lastname\">Nom : </label>"
+					+"<input type=\"text\" class=\"form-control\" name=\"lastname\">"
+					+"</div>"
+					+"</br>"
+					+"<div class=\"col-md-12\">"
+					+"<label for=\"firstname\">Prénom : </label>"
+					+"<input type=\"text\" class=\"form-control\" name=\"firstname\">"
+					+"</div>"
+					+"</br>"
+					+"<div class=\"col-md-12\">"
+					+"<input type=\"submit\" class=\"btn btn-primary\" name=\"BT-inscription\" value=\"Inscription\">"
+					+"</div>"
+					+"</form>"
+					+"</div>"
+					+"</body>"
+					+"</html>";
+		} catch (Exception e) {
+			System.out.println(e);
+			return "une erreur s'est produite lors de l'inscription";
+		}
+	}
+
+	/**
+	 * PUT method for updating or creating an instance of ServiceResource
+	 *
+	 * @param content
+	 *            representation for the resource
+	 */
+	@PUT
+	@Consumes(MediaType.TEXT_HTML)
+	public void putHtml(String content) {
+	}
+
+	@POST
+	@Consumes("application/x-www-form-urlencoded")
+	@Produces("text/plain")
+	public String postHandler(String content) {
+		return content;
+	}
+}
